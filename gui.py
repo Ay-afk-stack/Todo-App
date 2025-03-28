2be01db4d1f106ad2687422867b0419c06a2af9b
ay-afk-stack <pakhrin.ayush404>
2025-03-28 22:51:03 +0545

    implemented complete and exit

--- gui.py
+++ gui.py
@@ -4,10 +4,14 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values = functions.get_todos(), key='todos_key',enable_events=True, size=[45,10])
+list_box = sg.Listbox(values = functions.get_todos(), key='todos_key',enable_events=True, size = [45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-window = sg.Window('My To-do App',[[label],[input_box,add_button],[list_box, edit_button]],font = ('JetBrains Mono',12))
+layout = [[label],[input_box,add_button],[list_box, edit_button,complete_button],[exit_button]]
+
+window = sg.Window('My To-do App',layout,font = ('JetBrains Mono',12))
 
 while True:
     # Destructing events and values from window.read()
@@ -29,11 +33,22 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos_key'].update(values=todos)
+            
+        case 'Complete':
+            todo_to_complete = values['todos_key'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos_key'].update(values = todos)
+            window['todo'].update(value = "")
         
         case 'todos_key':
             window['todo'].update(value = values['todos_key'][0])
         
+        case 'Exit':
+            break
+        
         case sg.WIN_CLOSED:
             break
-
+print('Bye')
 window.close()
