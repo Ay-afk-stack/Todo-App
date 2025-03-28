ea19166553d0b9129817448917a6560ef75bd4f3
ay-afk-stack <pakhrin.ayush404>
2025-03-28 10:13:52 +0545

    implemented edit and display

--- gui.py
+++ gui.py
@@ -4,19 +4,35 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values = functions.get_todos(), key='todos_key',enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-do App',[[label],[input_box,add_button]],font = ('Helvetica',20))
+window = sg.Window('My To-do App',[[label],[input_box,add_button],[list_box, edit_button]],font = ('JetBrains Mono',12))
 
 while True:
     # Destructing events and values from window.read()
     event, values = window.read()
+    
     match event:
         case 'Add':
             todos = functions.get_todos()
             new_todos = values['todo']+'\n'
             todos.append(new_todos)
             functions.write_todos(todos)
-            print(f"{values['todo']} added!")
+            window['todos_key'].update(values=todos)
+            
+        case 'Edit':
+            todo = values['todos_key'][0]
+            new_todo = values['todo']+"\n"
+            todos = functions.get_todos()
+            index = todos.index(todo)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos_key'].update(values=todos)
+        
+        case 'todos_key':
+            window['todo'].update(value = values['todos_key'][0])
+        
         case sg.WIN_CLOSED:
             break
 
